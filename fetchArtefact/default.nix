7e9a08d5750ecb9015b2372bddb249ccf7055a08
Raphael Zimmermann <mail@raphael.li>
2024-05-25 18:23:22 +0200

    Remove useless let in

--- fetchArtefact/default.nix
+++ fetchArtefact/default.nix
@@ -5,27 +5,25 @@
   curl,
   nix,
   cacert,
-}: let
-in
-  {
-    # A list of URLs specifying alternative download locations. They are tried in order.
-    urls,
-    # SRI hash.
-    hash,
-    # Name of the file.
-    name,
-  }:
-    stdenvNoCC.mkDerivation {
-      inherit name hash;
-      outputHash = hash;
-      outputHashMode = "flat";
+}: {
+  # A list of URLs specifying alternative download locations. They are tried in order.
+  urls,
+  # SRI hash.
+  hash,
+  # Name of the file.
+  name,
+}:
+stdenvNoCC.mkDerivation {
+  inherit name hash;
+  outputHash = hash;
+  outputHashMode = "flat";
 
-      builder = ./builder.bash;
-      nativeBuildInputs = [curl nix];
-      SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
-      inherit urls;
+  builder = ./builder.bash;
+  nativeBuildInputs = [curl nix];
+  SSL_CERT_FILE = "${cacert}/etc/ssl/certs/ca-bundle.crt";
+  inherit urls;
 
-      # Doing the download on a remote machine just duplicates network
-      # traffic, so don't do that
-      preferLocalBuild = true;
-    }
+  # Doing the download on a remote machine just duplicates network
+  # traffic, so don't do that
+  preferLocalBuild = true;
+}
