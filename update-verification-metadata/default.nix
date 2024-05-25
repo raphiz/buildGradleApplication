c75d75c980ca839825c9607152818c96f3989be3
Raphael Zimmermann <mail@raphael.li>
2024-05-25 18:23:59 +0200

    Make verificationFile and cmd configurable

--- update-verification-metadata/default.nix
+++ update-verification-metadata/default.nix
@@ -3,23 +3,23 @@
   python3,
   git,
   writeShellApplication,
-}: let
-  verificationFile = "gradle/verification-metadata.xml";
-  cmd = "gradle --refresh-dependencies --write-verification-metadata sha256 prepareKotlinBuildScriptModel";
-in
-  writeShellApplication {
-    name = "update-verification-metadata";
-    runtimeInputs = [python3 gradle git];
-    text = ''
-      verificationFile=''${1:-gradle/verification-metadata.xml}
-      if [ ! -f "$verificationFile" ]
-      then
-        echo "Error: $verificationFile does not (yet) exist."
-        exit 1
-      fi
-      echo "Removing all component entries from $verificationFile ..."
-      python ${./update-verification-metadata.py} "$verificationFile"
-      echo "Regenerating gradle verification data ..."
-      ${cmd}
-    '';
-  }
+}: {
+  verificationFile ? "gradle/verification-metadata.xml",
+  cmd ? "gradle --refresh-dependencies --write-verification-metadata sha256 prepareKotlinBuildScriptModel",
+}:
+writeShellApplication {
+  name = "update-verification-metadata";
+  runtimeInputs = [python3 gradle git];
+  text = ''
+    verificationFile=''${1:-gradle/verification-metadata.xml}
+    if [ ! -f "$verificationFile" ]
+    then
+      echo "Error: $verificationFile does not (yet) exist."
+      exit 1
+    fi
+    echo "Removing all component entries from $verificationFile ..."
+    python ${./update-verification-metadata.py} "$verificationFile"
+    echo "Regenerating gradle verification data ..."
+    ${cmd}
+  '';
+}
