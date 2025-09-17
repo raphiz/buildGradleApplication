3f2653dc34ad8c3034233b8058b98318ef0c1816
Raphael Zimmermann <mail@raphael.li>
2025-09-17 12:56:00 +0200

    Update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix builder function for packaging Gradle applications";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = inputs @ {
